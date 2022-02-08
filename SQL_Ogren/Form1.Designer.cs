715f07c6ae71729ee91b5cf665c1ac105a73b81e
yigitcanolmez <yigitcanolmez@outlook.com>
2022-02-08 19:41:05 +0300

    Add project files.

---
+++ SQL_Ogren/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace SQL_Ogren
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(702, 272);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(720, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(306, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Veri Tabanı Çağır";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(720, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(306, 29);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(720, 91);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(306, 94);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Query Çalıştır";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(720, 191);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(306, 93);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Ekle Sil Güncelle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Enabled = false;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 299);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1014, 306);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1038, 617);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "SQL Panel";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ SQL_Ogren/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SQL_Ogren
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string db;
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            try
+            {
+                db = textBox1.Text;
+                SqlConnection bgl = new SqlConnection(@"Data Source=DESKTOP-EKQK6BH\SQLEXPRESS;Initial Catalog=" + db + ";Integrated Security=True");
+                richTextBox1.Enabled = true;
+                dataGridView1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                MessageBox.Show(db + " veritabanına bağlanıldı!");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Hatalı veri tabanı ismi ");
+                throw;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection bgl = new SqlConnection(@"Data Source=DESKTOP-EKQK6BH\SQLEXPRESS;Initial Catalog=" + db + ";Integrated Security=True");
+
+            bgl.Open();
+            string bglkomut;
+            bglkomut = richTextBox1.Text;
+            SqlDataAdapter da = new SqlDataAdapter(bglkomut, bgl);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            bgl.Close();
+
+
+
+        
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection bgl = new SqlConnection(@"Data Source=DESKTOP-EKQK6BH\SQLEXPRESS;Initial Catalog=" + db + ";Integrated Security=True");
+
+                bgl.Open();
+                string bglkomut1;
+                bglkomut1 = richTextBox1.Text;
+                SqlCommand komut = new SqlCommand(bglkomut1, bgl);
+
+                komut.ExecuteNonQuery();
+                bgl.Close();
+                MessageBox.Show("İşlem başarılı");
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("KOD HATASI!");
+            }
+            
+
+
+        }
+    }
+}
